4896f1c3e0ee831f00e1a88d964a62479f9a34a5
Jan Novak <novak.jan90@gmail.com>
2023-02-12 19:45:06 +0100

    refactor: Add NotFound message

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -63,7 +63,7 @@ export class UsersService {
     });
 
     if (!user) {
-      throw new NotFoundException();
+      throw new NotFoundException('User with this email does not exist.');
     }
 
     return user.id;
