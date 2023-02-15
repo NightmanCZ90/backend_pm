d00df32be439066d58eb2790dfd456b9b6bf8616
Jan Novak <novak.jan90@gmail.com>
2023-02-16 00:14:07 +0100

    feat: Implement createTransaction

--- src/transactions/transactions.controller.ts
+++ src/transactions/transactions.controller.ts
@@ -1,6 +1,22 @@
-import { Controller, UseGuards } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
+import { CurrentUser } from '../common/decorators';
 import { JwtGuard } from '../common/guards';
+import { CreateTransactionDto } from './dto';
+import { TransactionsService } from './transactions.service';
 
 @UseGuards(JwtGuard)
 @Controller('transactions')
-export class TransactionsController { }
+export class TransactionsController {
+  constructor(
+    private readonly transactionsService: TransactionsService
+  ) { }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  createTransaction(
+    @CurrentUser() user: Express.User,
+    @Body() dto: CreateTransactionDto,
+  ) {
+    return this.transactionsService.createTransaction(user.userId, dto);
+  }
+}
--- src/transactions/transactions.service.ts
+++ src/transactions/transactions.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { Transaction } from '@prisma/client';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateTransactionDto } from './dto';
 
 @Injectable()
-export class TransactionsService { }
+export class TransactionsService {
+  constructor(
+    private readonly prisma: PrismaService,
+  ) { }
+
+  async createTransaction(
+    userId: number,
+    dto: CreateTransactionDto,
+  ): Promise<Transaction> {
+    const portfolio = await this.prisma.portfolio.findUnique({
+      where: { id: dto.portfolioId }
+    });
+
+    if (!portfolio) {
+      throw new NotFoundException('Portfolio not found.');
+    }
+
+    if (portfolio.pmId && portfolio.pmId !== userId) {
+      throw new UnauthorizedException("Transaction creation is only allowed by its portfolio manager.");
+    }
+
+    if (!portfolio.pmId && portfolio.userId !== userId) {
+      throw new UnauthorizedException("You don't have permission to create transaction to this portfolio.");
+    }
+
+    const transaction = await this.prisma.transaction.create({
+      data: {
+        ...dto,
+      }
+    });
+
+    return transaction;
+  }
+}
